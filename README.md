d8e4a3f2ac56f89ebfdc838a1a1221f1ffecf43a
kat-ash59 <kkasmieh@hotmail.com>
2024-08-31 14:36:50 -0600

    adding changes for files

--- README.md
+++ README.md
@@ -1,9 +1,27 @@
-# Hi/Lo Game
+# FizzBuzz App
 
 ### Description
+Directly from the homework source....
+This program plays the game "Fizzbuzz". The program will count from 1 to 100, 
+replacing each multiple of 3 with the word "fizz", 
+each multiple of 5 with the word “buzz", 
+and each multiple of both with the word “fizzbuzz". 
 
+Note:
+Fizzbuzz is a common interview question!
+
+
+wooot woot I did it!!!
 
 ### Technologies Used
+if
+else if
+else
+
+modulus
+
+
 
 
 ### Lessons Learned
+need to be careful where you put the checks so that you get proper output for combinations 
--- src/fizzbuzz/FizzBuzzApp.java
+++ src/fizzbuzz/FizzBuzzApp.java
@@ -1,3 +1,30 @@
+// starting with
+// User Story #1
+// The program prints the numbers 1-100 to the console.
+
+// Woot Woot User Story 1 worked just fine
+
+// now onto
+// User Story #2
+// If a number is divisible by 3 replace the numeric console output with the word “fizz”.
+
+
+// woot woot User 2 works just like it is supposed to do
+
+// now on to
+// User Story #3
+// If a number is divisible by 5 replace the numeric console output with the word “buzz”.
+
+// now that was fun!! user story 3 worked as planned
+
+// drum roll please!!!!
+// now onto
+// User Story #4
+// If a number is divisible by both 3 and 5 replace the numeric console output with "fizzbuzz".
+
+// Yippiee Ki-Yay MF!!! it worked!!!!! i may still have a bit of a programmer left in me!
+
+
 package fizzbuzz;
 
 public class FizzBuzzApp 
@@ -5,6 +32,35 @@ public class FizzBuzzApp
 
 	public static void main(String[] args) 
 	{
+		// declare variables
+		int counter = 1;
+		
+		
+		// setting up while loop and initially going to just display print out of 1 to 100
+		while (counter <= 100)
+		{
+			// now adding code for printing fizz
+			// next adding code for printing buzz
+			// next adding code for fizzbuzz - note need to add it first so that it will print fizzbuzz instead of fizz or buzz
+			if (((counter % 3) == 0) && ((counter % 5) == 0))
+			{
+				System.out.println("fizzbuzz");
+			}
+			else if ((counter % 3) == 0)
+			{
+				System.out.println("fizz");
+				
+			}
+			else if ((counter % 5) == 0)
+			{
+				System.out.println("buzz");
+			}
+			else
+			{
+				System.out.println(counter);
+			}
+			counter ++;
+		} // end while loop
 		
 
 	} // end main
